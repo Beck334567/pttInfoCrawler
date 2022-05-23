ab93a1f57cdb75c458e1d8823925a066fca46980
Beck <sars88967@gmail.com>
2022-05-23 16:53:41 +0800

    remove

--- pttInfoCrawler/Data/Board.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace pttInfoCrawler.Model
-{
-    public class Board
-    {
-        public int BoardID { get; set; }
-        public string Name { get; set; }
-        public string Url { get; set; }
-    }
-}
